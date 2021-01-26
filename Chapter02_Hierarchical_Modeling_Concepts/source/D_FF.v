de0dd95fb6d0396114042fdbffaf76f839d24b02
HAMakaryan <hrach.makaryan@gmail.com>
2021-01-27 01:06:20 +0400

    Added Chapter02_Hierarchical_Modeling_Concepts/source/D_FF.v file

---
+++ Chapter02_Hierarchical_Modeling_Concepts/source/D_FF.v
@@ -0,0 +1,28 @@
+// module D_FF with synchronous reset
+module D_FF(
+  q,
+  d,
+  clk,
+  reset
+);
+
+output q;
+input d, clk, reset;
+
+reg q;
+
+// Lots of new constructs. Ignore the functionality of the constructs.
+// Concentrate on how the design block is built in a top-down fashion.
+
+always @(posedge reset or negedge clk)
+begin
+  if (reset)
+  begin
+    q <= 1'b0;
+  end else
+  begin
+    q <= d;
+  end
+end
+
+endmodule
