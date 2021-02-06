f3b81d481f5b4d87a03cc739a4b260ebff233e21
unknown <hrach.makaryan@gmail.com>
2021-02-06 06:46:03 -0800

    Ubdate teh test bench and schematics

--- Chapter06_Dataflow_Modeling/source/edge_dff_tb.v
+++ Chapter06_Dataflow_Modeling/source/edge_dff_tb.v
@@ -10,6 +10,7 @@ reg   d_r;
 reg   clk_r;
 reg   clear_r;
 integer scenario;
+reg   [8*40:1]comment;
 
 
 always
@@ -24,51 +25,99 @@ end
 
 initial
 begin
-  scenario= 0;
   d_r     = 1'b0;
   clear_r = 1'b0;
   #11;
+
+  // Scenario 10 reset
+  scenario= 10;
+  comment = "reset";
   clear_r = 1'b1;
   #11;
   clear_r = 1'b0;
+
+  // Scenario 20 d = 0
+  scenario= 20;
+  comment = "d = 0";
   repeat(2) @(posedge clk_r);
-  d_r     = 1'b1;
-  repeat(2) @(posedge clk_r);
-  // Scenario 1
-  scenario= 1;
-  @(posedge clk_r);
+
+  // Scenario 30 d = 1
+  scenario= 30;
+  comment = "d = 1";
   #0.5;
   d_r     = 1'b1;
-  @(negedge clk_r);
-  #0.5;
-  repeat (20) #0.1 d_r = !d_r;
-  // Scenario 2
-  scenario= 2;
-  @(posedge clk_r);
+  repeat(2) @(posedge clk_r);
+
+  // Scenario 40 d = 0
+  scenario= 40;
+  comment = "d = 0";
   #0.5;
   d_r     = 1'b0;
-  @(negedge clk_r);
-  #0.5;
-  repeat (20) #0.1 d_r = !d_r;
+  repeat(2) @(posedge clk_r);
 
-  // Scenario 3
-  scenario= 3;
+  // Scenario 50; d=0, clk=1, d=pulse, d=0
+  scenario= 50;
+  comment = "d=0, clk=1, d=pulse, d=0";
   @(negedge clk_r);
-  #0.5;
-  d_r     = 1'b1;
-  @(posedge clk_r);
-  #0.5;
-  repeat (20) #0.1 d_r = !d_r;
-  // Scenario 4
-  scenario= 4;
-  @(negedge clk_r);
-  #0.5;
+  #1;
   d_r     = 1'b0;
   @(posedge clk_r);
-  #0.5;
-  repeat (20) #0.1 d_r = !d_r;
+  #1;
+  repeat(20) #0.1 d_r = !d_r;
+  @(posedge clk_r);
+  #1;
+  repeat(20) #0.1 d_r = !d_r;
+  @(posedge clk_r);
+
+  // Scenario 60; d=1, clk=1, d=pulse, 1=0
+  scenario= 60;
+  comment = "d=1, clk=1, d=pulse, 1=0";
+  @(negedge clk_r);
+  #1;
+  d_r     = 1'b1;
+  @(posedge clk_r);
+  #1;
+  repeat(20) #0.1 d_r = !d_r;
+  @(posedge clk_r);
+  #1;
+  repeat(20) #0.1 d_r = !d_r;
+  @(posedge clk_r);
+
+////////////////////
+
+  // Scenario 70; d=0, clk=0, d=pulse, d=0
+  scenario= 70;
+  comment = "d=0, clk=0, d=pulse, d=0";
+  @(posedge clk_r);
+  #1;
+  d_r     = 1'b0;
+  @(negedge clk_r);
+  #1;
+  repeat(20) #0.1 d_r = !d_r;
+  @(negedge clk_r);
+  #1;
+  repeat(20) #0.1 d_r = !d_r;
+  @(posedge clk_r);
+
+  // Scenario 80; d=1, clk=1, d=pulse, 1=0
+  scenario= 80;
+  comment = "d=1, clk=1, d=pulse, 1=0";
+  @(posedge clk_r);
+  #1;
+  d_r     = 1'b1;
+  @(negedge clk_r);
+  #1;
+  repeat(20) #0.1 d_r = !d_r;
+  @(negedge clk_r);
+  #1;
+  repeat(20) #0.1 d_r = !d_r;
+
+
+
 
   repeat(2) @(posedge clk_r);
+
+
   $stop;
 end
 
